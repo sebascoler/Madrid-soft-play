1d90a7cf890b00acf299f4308dbf2e3a46173e76
sebascoler <93617201+sebascoler@users.noreply.github.com>
2026-07-21 14:45:39 +0100

    fix: remove basePath/assetPrefix for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-  basePath: '/Madrid-soft-play', 
-  assetPrefix: '/Madrid-soft-play/', // <-- ¡Esta línea es la clave!
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
